4d3b48477e655faec55e0709ae61f970e4347c90
Sir-Benj <40863501+Sir-Benj@users.noreply.github.com>
2018-09-27 20:07:08 +0100

    testing char model

---
+++ First Game - ARPG/Assets/Player/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public Rigidbody player;
+    public float speed = 1.2f;
+    public Animator animator;
+
+    void Start()
+    {
+        player.GetComponent<Rigidbody>();
+        animator.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        
+        animator.SetFloat("Speed", 0f);
+
+        if (input.x != 0 || input.z != 0)
+        {
+            animator.SetFloat("Speed", 0.75f);
+        }
+        player.MovePosition(transform.position + input * speed * Time.deltaTime);
+    }
+}
